1cfc14c68cafda3e1acd256659027c0efca193cf
Manzoor Wani <manzoorwani.jk@gmail.com>
2026-01-08 11:25:32 +0530

    Switch to pnpm

--- test/unit/jest.config.js
+++ test/unit/jest.config.js
@@ -14,12 +14,17 @@ process.env.TZ = 'UTC';
 module.exports = {
 	rootDir: '../../',
 	moduleNameMapper: {
-		[ `@wordpress\\/(${ transpiledPackageNames.join( '|' ) })$` ]:
-			'packages/$1/src',
+		// Specific mappings first (before generic patterns)
 		'@wordpress/vips/worker': '<rootDir>/packages/vips/src/vips-worker.ts',
 		'@wordpress/theme/design-tokens.js':
 			'<rootDir>/packages/theme/src/prebuilt/js/design-tokens.mjs',
 		'.+\\.wasm$': '<rootDir>/test/unit/config/wasm-stub.js',
+		// Map deep paths (e.g., @wordpress/block-editor/src/hooks/list-view)
+		[ `@wordpress\\/(${ transpiledPackageNames.join( '|' ) })\\/(.+)$` ]:
+			'packages/$1/$2',
+		// Then map exact package imports (e.g., @wordpress/block-editor)
+		[ `@wordpress\\/(${ transpiledPackageNames.join( '|' ) })$` ]:
+			'packages/$1/src',
 	},
 	preset: '@wordpress/jest-preset-default',
 	setupFiles: [
@@ -50,7 +55,9 @@ module.exports = {
 		'^.+\\.m?[jt]sx?$': '<rootDir>/test/unit/scripts/babel-transformer.js',
 	},
 	transformIgnorePatterns: [
-		'/node_modules/(?!(docker-compose|yaml|preact|@preact|parsel-js|comctx)/)',
+		// Match pnpm nested structure (.pnpm/pkg@version/node_modules/pkg)
+		// Packages that ship ESM and need to be transformed by Babel.
+		'/node_modules/.pnpm/(?!(docker-compose|yaml|preact@|@preact\\+signals|parsel-js|comctx))',
 		'\\.pnp\\.[^\\/]+$',
 	],
 	snapshotSerializers: [
