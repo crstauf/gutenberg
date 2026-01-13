890429fc47869d54d6e3ab113a8fc10e2118632e
ntsekouras <ntsekouras@outlook.com>
2026-01-13 10:24:59 +0200

    DataViews: Add overlay when loading to prevent flickering

--- packages/edit-site/src/components/post-list/index.js
+++ packages/edit-site/src/components/post-list/index.js
@@ -171,6 +171,7 @@ export default function PostList( { postType } ) {
 		isResolving: isLoadingData,
 		totalItems,
 		totalPages,
+		hasResolved,
 	} = useEntityRecordsWithPermissions( 'postType', postType, queryArgs );
 
 	const postIds = useMemo(
@@ -319,7 +320,9 @@ export default function PostList( { postType } ) {
 				fields={ fields }
 				actions={ actions }
 				data={ data || EMPTY_ARRAY }
-				isLoading={ isLoadingData || isLoadingNotesCount }
+				isLoading={
+					isLoadingData || isLoadingNotesCount || ! hasResolved
+				}
 				view={ view }
 				onChangeView={ onChangeView }
 				selection={ selection }
